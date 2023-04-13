20a2db3ffeaba6362f3cab246a560ba4b886fa52
“joelandongson” <joelonob77@gmail.com>
2023-04-12 21:30:32 -0600

    modify code

--- terraform/v1-EC2/v1-ec2.tf
+++ terraform/v1-EC2/v1-ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-0b0dcb5067f052a63"
+    ami = "ami-09ec3af8c9f2258df"
     instance_type = "t2.micro"
-    key_name = "prd01"
+    key_name = "rpt-03-key"
 }
