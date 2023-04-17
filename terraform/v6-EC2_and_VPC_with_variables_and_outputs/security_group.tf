4af01c2303f3f4f7789cd41032f0420f7fc48bcf
“joelandongson” <joelonob77@gmail.com>
2023-04-16 18:32:13 -0600

    updated security group ports

--- terraform/v6-EC2_and_VPC_with_variables_and_outputs/security_group.tf
+++ terraform/v6-EC2_and_VPC_with_variables_and_outputs/security_group.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "rtp03-sg" {
 
     }
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = 400
+        to_port = 4000
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
 
