9c96957dd24b011e39a0b8c1856105d95b49c28d
“joelandongson” <joelonob77@gmail.com>
2023-04-22 23:35:39 -0600

    updated v2-ec2 ami

--- terraform/v2-EC2_with_SG/v2-ec2_with_sg.tf
+++ terraform/v2-EC2_with_SG/v2-ec2_with_sg.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-09ec3af8c9f2258df"
+    ami = "ami-02396cdd13e9a1257"
     instance_type = "t2.micro"
     key_name = "mac-rtp-03"
     security_groups = ["rtp03-sg"]
